13f5548fd036fc8f08ab699d1d4817bdee61ab2d
hellojoelhuber <hello@joelhuber.com>
2022-06-18 15:07:24 -0500

    Added PlayerCreateDTO

---
+++ Sources/TicTacToeCore/APIModels/PlayerCreateDTO.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joel Huber on 6/18/22.
+//
+
+import Foundation
+
+public struct PlayerCreateDTO: Codable {
+    public let id: UUID
+    public let firstName: String
+    public let lastName: String
+    public let username: String
+    public let password: String
+    public let email: String
+    public let profileIcon: String
+
+    public init(id: UUID,
+                firstName: String, lastName: String,
+                username: String, password: String, email: String,
+                profileIcon: String = "hare") {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.username = username
+        self.password = password
+        self.email = email
+        self.profileIcon = profileIcon
+    }
+}
